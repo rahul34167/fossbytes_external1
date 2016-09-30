9cda713a353e9622c306da0e640db1a5c96f6ca4
rahul34167 <rahulagg25@gmail.com>
2016-09-30 23:40:45 +0530

    message

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/callWiki', function(req, res) {
 
   console.log("search string is" + search_string);
 
-      request.get({ url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+search_string+"&limit=2&format=json" },      function(error, response, body) { 
+      request.get({ url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+search_string+"&limit=2&format=json" },function(error, response, body) { 
               if (!error && response.statusCode == 200) { 
 
                   //res.json(body);
@@ -26,14 +26,20 @@ app.get('/callWiki', function(req, res) {
                  // res.send(parsed_json[2]);
                   var result_string =  JSON.stringify(parsed_json[2]);
                   var final_string = result_string.substring(1,result_string.length-1); 
-                  console.log(final_string);
 
-                var result = [];
-                if(final_string)
-                result.push({text: final_string});
+                  console.log(result_string);
+                  var result_notfound = "Result not found. Try looking for something else";
+                  var res_string = final_string.replace("\"","");
+                 // var global=JSON.parse("["+final_string+"]");
                   
-                res.contentType('application/json');
-                res.send(result);
+                  var result = [];
+                  if(final_string == "")
+                  result.push({text: result_notfound});
+                  else
+                  result.push({text: res_string}); 
+                    
+                  res.contentType('application/json');
+                  res.send(result);
 
                  } 
              }); 
